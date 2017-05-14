55966cd2a6c5e38f95d9f14286387637a6f1e7d9
Daniella Gerard <daniella4422@gmail.com>
2017-05-14 14:16:49 -0700

    final submitted version

--- Makefile
+++ Makefile
@@ -1,20 +1,24 @@
 NAME = fillit
 CFLAGS = -Wall -Wextra -Werror
-DEPS = libft.a
+DEPS = libft
 FUNCTIONS = main.c\
 			file_checker.c\
 			translator.c\
 			solver.c\
-			check_add_remove.c\
+			check_add_remove.c
+
+OBJ = $(FUNCTIONS:.c=.o)
 
 all: $(NAME)
 
-$(NAME):
-	gcc $(CFLAGS) -o $@ $(FUNCTIONS) libft.a
-
+$(NAME): $(OBJ)
+	gcc $(CFLAGS) -o $@ $(OBJ) $(DEPS).a
+%.o: %.c
+	gcc $(CFLAGS) -c $^ -o $@
 clean:
-	/bin/rm -f rm $(NAME)
+	/bin/rm -f rm $(OBJ)
 
 fclean: clean
+	/bin/rm -f rm $(NAME)
 
 re: fclean all
--- translator.c
+++ translator.c
@@ -6,76 +6,48 @@
 /*   By: esterna <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/04 12:34:15 by esterna           #+#    #+#             */
-/*   Updated: 2017/05/07 22:27:14 by dgerard          ###   ########.fr       */
+/*   Updated: 2017/05/09 18:02:10 by esterna          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "libfill.h"
 
-static void			ft_codegen(char *buf, int *dest)
+static void		ft_codegen(char *buf, int *dest, int k1)
 {
 	int i;
-	int k1;
 	int k2;
 	int r;
 	int hashtags;
 
 	i = 0;
-	k1 = 0;
-	k2 = 0;
 	r = 0;
 	hashtags = 0;
-	while (*buf != '#')
-	{
-		k1++;
-		buf++;
-	}
 	k2 = k1 + 1;
-	buf++;
-	while (*buf && hashtags < 3)
+	while (buf[k2] && hashtags < 3)
 	{
-		while (*buf != '\n' && *buf != '#' && *buf)
-		{
-			buf++;
+		while (buf[k2] != '\n' && buf[k2] != '#' && buf[k2])
 			k2++;
-		}
-		if (*buf == '#')
-			hashtags++;
-		if (*buf == '\n')
-		{
+		if (buf[k2] == '\n')
 			r++;
-			k2++;
-			buf++;
-		}
 		else
 		{
+			hashtags++;
 			dest[i++] = r;
-			if (r > 0)
-			{
-				if ((k2 % 5) - (k1 % 5) < 0)
-					dest[i++] = -((k1 % 5) - (k2 % 5));
-				else
-					dest[i++] = ((k2 % 5) - (k1 % 5));
-			}
-			else
-			{
-				if (k2 - k1 < 0)
-					dest[i++] = -(k1 - k2);
-				else
-					dest[i++] = (k2 - k1);
-			}
-			k2++;
-			buf++;
+			dest[i++] = (r > 0) ? ((k2 % 5) - (k1 % 5)) : (k2 - k1);
 		}
+		k2++;
 	}
 }
-static int	read_loop(int fd, int **lst)
+
+static int		read_loop(int fd, int **lst)
 {
 	int		i;
+	int		k1;
 	char	*buf;
 
 	i = 0;
+	k1 = 0;
 	buf = (char *)malloc(sizeof(char) * 21);
 	while (read(fd, buf, 21) > 0)
 	{
@@ -85,13 +57,17 @@ static int	read_loop(int fd, int **lst)
 			free(lst);
 			return (0);
 		}
-		ft_codegen(buf, lst[i]);
+		while (buf[k1] != '#')
+			k1++;
+		ft_codegen(buf, lst[i], k1);
 		i++;
+		k1 = 0;
 	}
 	free(buf);
 	return (i);
 }
-int			**translator(char *file, int num_tetri)
+
+int				**translator(char *file, int num_tetri)
 {
 	int		i;
 	int		fd;
